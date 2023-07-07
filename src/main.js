5f0645d868fdec8ecae5ce21a6711e3ccfed5078
leetxt13 <leetxt13@naver.com>
2023-07-07 10:44:13 +0900

    JAVASCRIPT-NAVBAR, TOGGLE

--- src/main.js
+++ src/main.js
@@ -4,16 +4,43 @@ const headerHeight = header.offsetHeight;
 //offsetHeight는 높이를 정수값으로 표현
 
 document.addEventListener("scroll", () => {
-  if (headerHeight < window.scrollY / 2) {
+  if (headerHeight < window.scrollY / 3) {
     header.classList.add("header--darker");
-  } else if (headerHeight > window.scrollY / 2) {
+  } else if (headerHeight > window.scrollY / 3) {
     header.classList.remove("header--darker");
   }
 });
 //home 섹션을 아래로 스크롤시 투명하게
 const home = document.querySelector("#home .max-container");
 const homeHeight = home.getBoundingClientRect().height;
-console.log(homeHeight);
+
 document.addEventListener("scroll", () => {
   home.style.opacity = 1 - scrollY / homeHeight;
+});
+
+//화살표 버튼 스코롤시 home에서는 안 보이게
+const wholeHome = document.querySelector("#home");
+const wholeHomeHeight = wholeHome.offsetHeight;
+const arrow = document.querySelector(".arrow-up");
+
+document.addEventListener("scroll", () => {
+  if (scrollY > wholeHomeHeight / 2) {
+    arrow.style.opacity = 1;
+  } else {
+    arrow.style.opacity = 0;
+  }
+});
+
+// toggle버튼을 누르면 nav메뉴 보이고, 사라지게
+const toggleBtn = document.querySelector(".header__toggleBtn");
+const headerMenu = document.querySelector(".header__menu");
+
+toggleBtn.addEventListener("click", () => {
+  headerMenu.classList.toggle("open");
+});
+
+headerMenu.addEventListener("click", (event) => {
+  if (event.target.nodeName === "A") {
+    headerMenu.classList.remove("open");
+  }
 });
