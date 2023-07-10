8bcf701703dba661e60fd3439c7022c380a9ef17
leetxt13 <leetxt13@naver.com>
2023-07-10 20:26:17 +0900

    type added2

--- src/typing.js
+++ src/typing.js
@@ -1,6 +1,6 @@
 "use strict";
 
-new TypeIt(".home__title--strong", { loop: true, speed: 100 }) //joy
+new TypeIt(".home__title--strong", { loop: true, speed: 110 }) //joy
   .move(-3)
   .type("Amazing ") // Amazing |joy
   .pause(1500)
@@ -8,6 +8,12 @@ new TypeIt(".home__title--strong", { loop: true, speed: 100 }) //joy
   .delete()
   .type("funny Joy") //funny Joy|
   .pause(1500)
+  .move(-4)
+  .delete(5) // funny| Joy
+  .type("happy")
+  .pause(1500)
+  .move(null, { to: "END" })
+  .delete()
   //   .move(-4)
   //   .delete(-5)
   //   .type("happy Joy")
