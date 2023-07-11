74bb565883d9c2835cbab560583b5e5270fbf220
leetxt13 <leetxt13@naver.com>
2023-07-11 10:51:10 +0900

    finish

--- src/typing.js
+++ src/typing.js
@@ -4,7 +4,7 @@ new TypeIt(".home__title--strong", { loop: true, speed: 110 }) //joy
   .move(-3)
   .type("Amazing ") // Amazing |joy
   .pause(1500)
-  .move(null, { to: "END" })
+  .move(null, { to: "END" }) // 끝으로 이동
   .delete()
   .type("funny Joy") //funny Joy|
   .pause(1500)
@@ -14,8 +14,4 @@ new TypeIt(".home__title--strong", { loop: true, speed: 110 }) //joy
   .pause(1500)
   .move(null, { to: "END" })
   .delete()
-  //   .move(-4)
-  //   .delete(-5)
-  //   .type("happy Joy")
-  //   .delete()
   .go();
